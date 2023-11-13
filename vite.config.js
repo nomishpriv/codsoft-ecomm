8784653eaa490e5df7451ea2d50768088c461485
your_name <nomanwahid123@gmail.com>
2023-11-14 01:34:45 +0500

    fix: some bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/codsoft-ecomm',
+  base: '/codsoft-ecomm/',
   plugins: [react()],
 })
