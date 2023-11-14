9054344ecfee14ae0ab4c844765f92b5dc3abd08
your_name <nomanwahid123@gmail.com>
2023-11-14 21:43:23 +0500

    fix: some bug

--- src/components/ProductCarousel.jsx
+++ src/components/ProductCarousel.jsx
@@ -7,7 +7,7 @@ const ProductCarousel = () => {
     <Carousel pause="hover" className="bg-primary mb-4">
       {products.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
+          <Link to={`/codsoft-ecomm/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className="carousel-caption">
               <h2>
