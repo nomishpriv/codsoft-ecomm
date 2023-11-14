b3d354408b469b7f2846dd1c153498e981259e10
your_name <nomanwahid123@gmail.com>
2023-11-14 22:10:58 +0500

    fix: some bug

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -41,7 +41,7 @@ const ProductScreen = () => {
 
   return (
     <>
-      <Link className="btn btn-light my-3" to="/">
+      <Link className="btn btn-light my-3" to="/codsoft-ecomm/">
         Go Back
       </Link>
 
